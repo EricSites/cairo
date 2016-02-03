28119a76b64b8f19f98f703824d084b3695b50e8
Eric Sites <eric@meros.io>
2016-02-03 11:50:41 -0500

    Add keg

---
+++ cairo.rb
@@ -0,0 +1,78 @@
+class Cairo < Formula
+  desc "Vector graphics library with cross-device output support"
+  homepage "http://cairographics.org/"
+  url "http://cairographics.org/snapshots/cairo-1.15.2.tar.xz"
+  sha256 "268cc265a7f807403582f440643064bf52896556766890c8df7bad02d230f6c9"
+
+  bottle do
+    sha256 "0b6d60becf48ba6971ee9103e33ee8b9c96b394463483947657c9e45be28c566" => :el_capitan
+    sha256 "94d50f7d5c35559ba68f42049f3e4ac3eab1ff28c2adfad568495ec519acdbd3" => :yosemite
+    sha256 "d222f5a885755b21f56a8a141adf57f2e77b59f38529dcabbc04a57717ab692b" => :mavericks
+  end
+
+  keg_only :provided_pre_mountain_lion
+
+  option :universal
+
+  depends_on "pkg-config" => :build
+  depends_on :x11 => :optional if MacOS.version > :leopard
+  depends_on "freetype"
+  depends_on "fontconfig"
+  depends_on "libpng"
+  depends_on "pixman"
+  depends_on "glib"
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-gobject=yes
+      --enable-svg=yes
+      --enable-tee=yes
+      --enable-quartz-image
+    ]
+
+    if build.with? "x11"
+      args << "--enable-xcb=yes" << "--enable-xlib=yes" << "--enable-xlib-xrender=yes"
+    else
+      args << "--enable-xcb=no" << "--enable-xlib=no" << "--enable-xlib-xrender=no"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <cairo.h>
+      int main(int argc, char *argv[]) {
+        cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 600, 400);
+        cairo_t *context = cairo_create(surface);
+        return 0;
+      }
+    EOS
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    libpng = Formula["libpng"]
+    pixman = Formula["pixman"]
+    flags = (ENV.cflags || "").split + (ENV.cppflags || "").split + (ENV.ldflags || "").split
+    flags += %W[
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/cairo
+      -I#{libpng.opt_include}/libpng16
+      -I#{pixman.opt_include}/pixman-1
+      -L#{lib}
+      -lcairo
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
